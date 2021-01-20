73873f51864e96d8000e9feeef0759baae39732a
Pedro <a2019114887@estudiantes.edu.bo>
2021-01-19 21:35:39 -0400

    Commit-Proyect-Completo

---
+++ WebApipersonaSol/WebApipersona/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApipersona.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebApipersona.Models.Person> People { get; set; }
+    }
+}
